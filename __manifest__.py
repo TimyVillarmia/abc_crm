db77c88f2bc8ca1f20cb1f9678e6fab9de25cea4
Lemonhue <lemonhue54@gmail.com>
2026-07-02 13:50:35 +0800

    chore: include sales team as dependency

--- __manifest__.py
+++ __manifest__.py
@@ -5,14 +5,15 @@
     "license": "OEEL-1",
     "depends": [
         "crm",
+        "sales_team",
         "base",
     ],
     "data": [
         "data/res.region.csv",
         "data/config_parameters.xml",
         "views/crm_lead_views.xml",
-        "security/ir.model.access.csv",
         "security/res_groups.xml",
+        "security/ir.model.access.csv",
         "data/lost_reason_data.xml",
     ],
     "installable": True,
